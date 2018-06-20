54669eca0c70729e62676d26c3fae1841c86f685
fmoreira <fjmoreira@miuandes.cl>
2018-06-20 19:11:45 -0400

    fix log in

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,4 +22,4 @@ Pizza.create(recipe_id:3,crust:"Delgada")
 Pizza.create(recipe_id:4,crust:"Delgada")
 Pizza.create(recipe_id:5,crust:"Delgada")
 
-
+User.create(email:"user@user.com",password:"123456",password_confirmation:"123456")
